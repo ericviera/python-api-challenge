4c5ffacea15edb5c3dc8f4f5b51c99c98d91d4d8
Eric Viera <ericviera@Erics-MBP.home>
2020-07-20 23:41:57 -0400

    Second Commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0a3452169b571fd14ded4d6e11e3e12d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBAJVBdzenQqlcNLppcx8YdlnMJtubqcoM"
